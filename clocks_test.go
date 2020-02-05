54e6d8cfd047b2b7e2177f847369a99b34dab2ee
jhamPac <cloud@kevia.me>
2020-02-04 19:15:33 -0700

    initial setup and locking in deps

---
+++ clocks_test.go
@@ -0,0 +1,19 @@
+package clocks
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gypsydave5/learn-go-with-tests/math/v1/clockface"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
