576554d38a63cc33fef5fd6b261fdfe640cee423
jhamPac <cloud@kevia.me>
2020-02-09 20:53:36 -0700

    using const for reusable variables

--- clockface.go
+++ clockface.go
@@ -5,6 +5,10 @@ import (
 	"time"
 )
 
+const secondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 // Point is a point or hand on the clockface
 type Point struct {
 	X, Y float64
@@ -13,9 +17,9 @@ type Point struct {
 // SecondHand is the unit vector of the second hand of an analogue clock at time 't' represented as a Point
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
-	p = Point{p.X * 90, p.Y * 90}   // scale
-	p = Point{p.X, -p.Y}            // flip
-	p = Point{p.X + 150, p.Y + 150} // translate
+	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
+	p = Point{p.X, -p.Y}                                      // flip
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}         // translate
 	return p
 }
 
