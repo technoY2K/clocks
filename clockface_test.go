f8067fa75af9dc8d06adb7f8111d9eabd43c04aa
jhamPac <cloud@kevia.me>
2020-02-09 20:42:37 -0700

    scale, flip, translate

--- clockface_test.go
+++ clockface_test.go
@@ -60,7 +60,7 @@ func TestSecondHandVector(t *testing.T) {
 func TestSecondHandAt30Seconds(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := Point{X: 150, Y: 150 + 90}
+	want := Point{X: 150, Y: 150 - 90}
 	got := SecondHand(tm)
 
 	if got != want {
