1a6f010e7650d33bbe42c91996147e8c5ba46d6f
jhamPac <cloud@kevia.me>
2020-02-11 20:01:52 -0700

    writing tests for minute hand now

--- clockface_test.go
+++ clockface_test.go
@@ -69,6 +69,32 @@ func TestSVGWriterSecondHand(t *testing.T) {
 	}
 }
 
+func TestSVGWriterMinuteHand(t *testing.T) {
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{
+			simpleTime(0, 0, 0),
+			Line{150, 150, 150, 70},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			b := bytes.Buffer{}
+			SVGWriter(&b, c.time)
+
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
 		time  time.Time
