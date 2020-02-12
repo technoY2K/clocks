81195d633b1d1a27471f1b3d04a1da2b390e56d0
jhamPac <cloud@kevia.me>
2020-02-11 19:11:44 -0700

    added a svg writer test

--- clockface.go
+++ clockface.go
@@ -1,6 +1,7 @@
 package clockface
 
 import (
+	"encoding/xml"
 	"math"
 	"time"
 )
@@ -12,6 +13,32 @@ const clockCenterY = 150
 // Point is a point or hand on the clockface
 type Point struct {
 	X, Y float64
+}
+
+// Svg is the result of parsing it
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
 
 // SecondHand is the unit vector of the second hand of an analogue clock at time 't' represented as a Point
--- clockface_test.go
+++ clockface_test.go
@@ -1,6 +1,8 @@
 package clockface
 
 import (
+	"bytes"
+	"encoding/xml"
 	"math"
 	"testing"
 	"time"
@@ -15,6 +17,27 @@ func TestSecondHandAtMidnight(t *testing.T) {
 	if got != want {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
+}
+
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	b := bytes.Buffer{}
+	clockface.SVGWriter(&b, tm)
+
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	x2 := "150"
+	y2 := "60"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
+	}
+
+	t.Errorf("Expected to find the second hand with x2 of %+v and y2 of %+v, in the SVG output %v", x2, y2, b.String())
 }
 
 func TestSecondsInRadians(t *testing.T) {
