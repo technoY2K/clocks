f3cc7c0b3d0e70ddfdeede2026fae0e5389dfedc
jhamPac <cloud@kevia.me>
2020-02-16 10:13:11 -0700

    was able to move files confidently with the tests in place

--- clockface.go
+++ clockface.go
@@ -1,19 +1,8 @@
 package clockface
 
 import (
-	"encoding/xml"
-	"fmt"
-	"io"
 	"math"
 	"time"
-)
-
-const (
-	secondHandLength = 90
-	minuteHandLength = 80
-	hourHandLength   = 50
-	clockCenterX     = 150
-	clockCenterY     = 150
 )
 
 const (
@@ -25,66 +14,12 @@ const (
 	hoursInClock       = 2 * hourInHalfClock
 )
 
-const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg xmlns="http://www.w3.org/2000/svg"
-     width="100%"
-     height="100%"
-     viewBox="0 0 300 300"
-     version="2.0">`
-
-const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
-
-const svgEnd = `</svg>`
-
 // Point is a point or hand on the clockface
 type Point struct {
 	X, Y float64
 }
 
-// SVG for unmarsheled XML
-type SVG struct {
-	XMLName xml.Name `xml:"svg"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-	ViewBox string   `xml:"viewBox,attr"`
-	Version string   `xml:"version,attr"`
-	Circle  Circle   `xml:"circle"`
-	Line    []Line   `xml:"line"`
-}
-
-// Circle is a property of a clockface SVG
-type Circle struct {
-	Cx float64 `xml:"cx,attr"`
-	Cy float64 `xml:"cy,attr"`
-	R  float64 `xml:"r,attr"`
-}
-
-// Line is a property of a clockface SVG
-type Line struct {
-	X1 float64 `xml:"x1,attr"`
-	Y1 float64 `xml:"y1,attr"`
-	X2 float64 `xml:"x2,attr"`
-	Y2 float64 `xml:"y2,attr"`
-}
-
-// SVGWriter outputs an SVG with given arguments
-func SVGWriter(w io.Writer, t time.Time) {
-	io.WriteString(w, svgStart)
-	io.WriteString(w, bezel)
-	secondHand(w, t)
-	minuteHand(w, t)
-	hourHand(w, t)
-	io.WriteString(w, svgEnd)
-}
-
-func secondHand(w io.Writer, t time.Time) {
-	p := makeHand(secondHandPoint(t), secondHandLength)
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#f00;stroke-width:3px;"/>`, p.X, p.Y)
-}
-
-func secondHandPoint(t time.Time) Point {
+func SecondHandPoint(t time.Time) Point {
 	return angleToPoint(secondsInRadians(t))
 }
 
@@ -92,12 +27,7 @@ func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (secondsInHalfClock / (float64(t.Second()))))
 }
 
-func minuteHand(w io.Writer, t time.Time) {
-	p := makeHand(minuteHandPoint(t), minuteHandLength)
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:3px;"/>`, p.X, p.Y)
-}
-
-func minuteHandPoint(t time.Time) Point {
+func MinuteHandPoint(t time.Time) Point {
 	return angleToPoint(minutesInRadians(t))
 }
 
@@ -105,23 +35,12 @@ func minutesInRadians(t time.Time) float64 {
 	return (secondsInRadians(t) / minutesInClock) + (math.Pi / (minutesInHalfClock / float64(t.Minute())))
 }
 
-func hourHand(w io.Writer, t time.Time) {
-	p := makeHand(hourHandPoint(t), hourHandLength)
-	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:3px;"/>`, p.X, p.Y)
-}
-
-func hourHandPoint(t time.Time) Point {
+func HourHandPoint(t time.Time) Point {
 	return angleToPoint(hoursInRadians(t))
 }
 
 func hoursInRadians(t time.Time) float64 {
 	return ((minutesInRadians(t) / hoursInClock) + (math.Pi / (hourInHalfClock / float64(t.Hour()%12))))
-}
-
-func makeHand(p Point, length float64) Point {
-	p = Point{p.X * length, p.Y * length}
-	p = Point{p.X, -p.Y}
-	return Point{p.X + clockCenterX, p.Y + clockCenterY}
 }
 
 func angleToPoint(angle float64) Point {
--- clockface_test.go
+++ clockface_test.go
@@ -1,42 +1,10 @@
 package clockface
 
 import (
-	"bytes"
-	"encoding/xml"
 	"math"
 	"testing"
 	"time"
 )
-
-func TestSVGWriterSecondHand(t *testing.T) {
-	cases := []struct {
-		time time.Time
-		line Line
-	}{
-		{
-			simpleTime(0, 0, 0),
-			Line{150, 150, 150, 60},
-		},
-		{
-			simpleTime(0, 0, 30),
-			Line{150, 150, 150, 240},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			b := bytes.Buffer{}
-			SVGWriter(&b, c.time)
-
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
-			}
-		})
-	}
-}
 
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
@@ -70,35 +38,9 @@ func TestSecondHandPoint(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
-			got := secondHandPoint(c.time)
+			got := SecondHandPoint(c.time)
 			if !roughlyEqualPoint(got, c.point) {
 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
-			}
-		})
-	}
-}
-
-func TestSVGWriterMinutedHand(t *testing.T) {
-	cases := []struct {
-		time time.Time
-		line Line
-	}{
-		{
-			simpleTime(0, 0, 0),
-			Line{150, 150, 150, 70},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			b := bytes.Buffer{}
-			SVGWriter(&b, c.time)
-
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
 			}
 		})
 	}
@@ -133,35 +75,9 @@ func TestMinutedHandPoint(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
-			got := minuteHandPoint(c.time)
+			got := MinuteHandPoint(c.time)
 			if !roughlyEqualPoint(got, c.point) {
 				t.Fatalf("wanted %v Point, but got %v", c.point, got)
-			}
-		})
-	}
-}
-
-func TestSVGWriterHourHand(t *testing.T) {
-	cases := []struct {
-		time time.Time
-		line Line
-	}{
-		{
-			simpleTime(6, 0, 0),
-			Line{150, 150, 150, 200},
-		},
-	}
-
-	for _, c := range cases {
-		t.Run(testName(c.time), func(t *testing.T) {
-			b := bytes.Buffer{}
-			SVGWriter(&b, c.time)
-
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-				t.Errorf("Expected to find the hour hand line %+v, in the SVG lines %+v", c.line, svg.Line)
 			}
 		})
 	}
@@ -203,7 +119,7 @@ func TestHourHandPoint(t *testing.T) {
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
-			got := hourHandPoint(c.time)
+			got := HourHandPoint(c.time)
 			if !roughlyEqualPoint(got, c.point) {
 				t.Fatalf("Wanted %v Point, but got %v", c.point, got)
 			}
@@ -226,14 +142,4 @@ func roughlyEqualFloat64(a, b float64) bool {
 
 func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
-}
-
-func containsLine(l Line, ls []Line) bool {
-	for _, line := range ls {
-		if line == l {
-			return true
-		}
-	}
-
-	return false
 }
